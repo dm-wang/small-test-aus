bb592924db2abb2befba9dd1711e3294165be0e4
dmwang <dmwang@users.noreply.github.com>
2022-02-21 16:07:49 +1000

    add deepEqual function

--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,51 @@ export type JSONParseable = JSONObject | JSONArray | JSONPrimitive | Date;
  * @param u
  */
 export async function asyncDeepEquals(o: JSONParseable, u: JSONParseable): Promise<boolean> {
-  return o === u;
+  return isEqual(o, u);
 }
+
+const isEqual = (x: JSONParseable, y: JSONParseable) => {
+  // primitive types
+  if (x === y) {
+    return true;
+  } else if (
+    // if not primitive types
+    x instanceof Object &&
+    y instanceof Object
+  ) {
+    // if date
+    if (x.constructor === Date && y.constructor === Date) {
+      return x.getTime() === y.getTime();
+    }
+
+    // if array or object
+    const keysOfX = Object.keys(x);
+    const keysOfY = Object.keys(y);
+    if (keysOfX.length !== keysOfY.length) {
+      return false;
+    }
+
+    // array
+    if (x.constructor === Array && y.constructor === Array) {
+      x.sort();
+      y.sort();
+
+      for (let i = 0; i < keysOfX.length; i++) {
+        if (!isEqual(x[i], y[i])) return false;
+      }
+    }
+
+    // object
+    for (const key of keysOfX) {
+      if (!isEqual((x as JSONObject)[key], (y as JSONObject)[key])) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+  return false;
+};
 
 const reviver = (key: string, value: unknown) => {
   if (typeof value === 'string') {
